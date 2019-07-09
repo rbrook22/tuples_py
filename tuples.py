60c7a9199f23e56b1e15856d0119ed021df107b4
Richard Brooks <rickbrooks22@gmail.com>
2019-07-09 10:03:06 -0400

    created tuple and print statment

---
+++ tuples.py
@@ -0,0 +1,12 @@
+#Making my first tuple
+
+family = ("Rick", "Debbie", "Kendall", "Ricky", "Sydney", "Debbie - Step Mom", "Elaine")
+
+print(family)
+
+
+###
+# Trying to get this print statement to work with formating or interpolation but keep getting a syntax error. 
+# print("This is the list of my family members and their significant others: "{}.format(family))
+###
+
